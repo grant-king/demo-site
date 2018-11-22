47e83eb0bf8cee8e2fbe90b4a5d4804c2be583ba
Grant King <grantryan.king@gmail.com>
2018-11-21 16:03:43 -0800

    feat: Add blog urls to url patterns
    Include path to blog as '[domain]/articles/' url.

--- django_project/yousite/urls.py
+++ django_project/yousite/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+#from .blog import views
 
 urlpatterns = [
+    #path('/', views.main_home, name='main-home' ),
     path('admin/', admin.site.urls),
+    path('articles/', include('blog.urls'))
 ]
