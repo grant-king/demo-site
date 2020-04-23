c8c5dd3033e2260cebf7606f25d789909ef0a56c
Grant King <grantryan.king@gmail.com>
2020-04-22 22:24:28 -0700

    Update project
    Bulk update of all changed and new project files.
    
    Add several user features.

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
     if request.method == 'POST':
@@ -8,8 +9,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
-            return redirect('blog-home')
+            messages.success(request, f'Account created for {username}. You can now log in.')
+            return redirect('login')
     else:
         form = UserRegisterForm()
 
@@ -17,3 +18,15 @@ def register(request):
          'form':form
      }
     return render(request, 'users/register.html', context=context)
+
+@login_required
+def profile(request):
+    u_form = UserUpdateForm()
+    p_form = ProfileUpdateForm()
+
+    context = {
+        'u_form': u_form, 
+        'p_form': p_form
+    }
+
+    return render(request, 'users/profile.html', context)
