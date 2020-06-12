ed4e31cb2d85439b875929fa6a315deee23450ce
Grant King <grantryan.king@gmail.com>
2020-06-11 19:51:05 -0700

    Enable S3 storage
    Register storages app, add required constants to settings.py.
    
    Comment out unneded image save() method from Project model.

--- working_project/mysite/models.py
+++ working_project/mysite/models.py
@@ -23,16 +23,15 @@ class Project(models.Model): #portfolio item data structure
     def __str__(self):
         return self.title
 
-    def save(self, *args, **kwargs):
-        MAX_SIZE = (500, 500) #height, width
-
-        super().save(self, *args, **kwargs)
-        img = Image.open(self.image.path)
-
-        if img.height > MAX_SIZE[0] or img.width > MAX_SIZE[1]:
-            size = (img.height, img.width)
-            resize_ratio = min(MAX_SIZE[0] / size[0], MAX_SIZE[1] / size[1])
-            new_size = [size[0] * resize_ratio, size[1] * resize_ratio]
-            img.thumbnail(new_size)
-            img.save(self.image.path)
-
+#    def save(self, *args, **kwargs):
+#        MAX_SIZE = (1080, 1920) #height, width
+#
+#        super().save(*args, **kwargs)
+#        img = Image.open(self.image.path)
+#
+#        if img.height > MAX_SIZE[0] or img.width > MAX_SIZE[1]:
+#            size = (img.height, img.width)
+#            resize_ratio = min(MAX_SIZE[0] / size[0], MAX_SIZE[1] / size[1])
+#            new_size = [size[0] * resize_ratio, size[1] * resize_ratio]
+#            img.thumbnail(new_size)
+#            img.save(self.image.path)
